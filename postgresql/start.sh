82849277f5fe4ddf13cee2a2a70f21797b0c327d
Pedro Coca <pedro.coca@gmail.com>
2021-02-07 17:15:08 +0000

    Adding a 1 second sleep to let postgres start up

--- postgresql/start.sh
+++ postgresql/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+POSTGRES_ADDR=localhost
+POSTGRES_PORT=5432
+POSTGRES_REPLY=52
 docker network create app-tier --driver bridge
 docker run --name my-postgres -d -p 5432:5432 --network app-tier pcoca/postgres:latest
+sleep 1
 PGPASSWORD=vaulttest psql -h localhost -p 5432 pgdbdemo -U postgres
